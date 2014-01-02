fa5f29fd75e107fedf5e2a5532465e36023bfdd7
eddy03 <eddytech03@gmail.com>
2014-01-02 09:14:52 +0800

    fix sending email before payment
    Fix sending email before payment. Issue #306413

--- catalog/controller/payment/molpay.php
+++ catalog/controller/payment/molpay.php
@@ -10,45 +10,46 @@
 class ControllerPaymentMolpay extends Controller {
     
     protected function index() {
-        $this->data['button_confirm'] = $this->language->get('button_confirm');
-        $this->data['button_back'] = $this->language->get('button_back');
-
-        $this->load->model('checkout/order');
-
-        $order_info = $this->model_checkout_order->getOrder($this->session->data['order_id']);
-
-        $this->data['action'] = 'https://www.onlinepayment.com.my/MOLPay/pay/'.$this->config->get('molpay_merchantid').'/';
-        $this->data['amount'] = $this->currency->format($order_info['total'], $order_info['currency_code'], $order_info['currency_value'], false);
-        $this->data['orderid'] = $this->session->data['order_id'];
-        $this->data['bill_name'] = $order_info['payment_firstname'] . ' ' . $order_info['payment_lastname'];
-        $this->data['bill_email'] = $order_info['email'];
-        $this->data['bill_mobile'] = $order_info['telephone'];
-        $this->data['country'] = $order_info['payment_iso_code_2'];
-        $this->data['currency'] = $order_info['currency_code'];
-        $this->data['vcode'] = md5($this->data['amount'].$this->config->get('molpay_merchantid').$this->data['orderid'].$this->config->get('molpay_verifykey'));
-
-        $products = $this->cart->getProducts();
-        foreach ($products as $product) {
-            $this->data['prod_desc'][]= $product['name']." x ".$product['quantity'];
-        }
+            $this->data['button_confirm'] = $this->language->get('button_confirm');
+            
+            $this->load->model('checkout/order');
 		
-        $this->data['order_id'] = $this->session->data['order_id'];
+            $order_info = $this->model_checkout_order->getOrder($this->session->data['order_id']);
+		
+            $this->data['action'] = 'https://www.onlinepayment.com.my/MOLPay/pay/'.$this->config->get('molpay_merchantid').'/';
+            
+            $this->data['amount'] = $this->currency->format($order_info['total'], $order_info['currency_code'], $order_info['currency_value'], false);
+            $this->data['orderid'] = $this->session->data['order_id'];
+            $this->data['bill_name'] = $order_info['payment_firstname'] . ' ' . $order_info['payment_lastname'];
+            $this->data['bill_email'] = $order_info['email'];
+            $this->data['bill_mobile'] = $order_info['telephone'];
+            $this->data['country'] = $order_info['payment_iso_code_2'];
+            $this->data['currency'] = $order_info['currency_code'];
+            $this->data['vcode'] = md5($this->data['amount'].$this->config->get('molpay_merchantid').$this->data['orderid'].$this->config->get('molpay_verifykey'));
+            
+            $products = $this->cart->getProducts();
+            foreach ($products as $product) {
+                $this->data['prod_desc'][]= $product['name']." x ".$product['quantity'];
+            }
 
-        $this->data['returnurl'] = HTTPS_SERVER.'index.php?route=payment/molpay/callback';
+            $this->data['order_id'] = $this->session->data['order_id'];
+            
+            $this->data['lang'] = $this->session->data['language'];
 
-        if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/payment/molpay.tpl')) {
-            $this->template = $this->config->get('config_template') . '/template/payment/molpay.tpl';
-        } else {
-            $this->template = 'default/template/payment/molpay.tpl';
-        }
-        $this->render();
-
-        $this->model_checkout_order->confirm($this->data['orderid'], $this->config->get('molpay_order_status_id'), 'Create Order', false);
+            $this->data['returnurl'] = $this->url->link('payment/molpay/callback', '', 'SSL');
+		
+            if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/payment/molpay.tpl')) {
+                    $this->template = $this->config->get('config_template') . '/template/payment/molpay.tpl';
+            } else {
+                    $this->template = 'default/template/payment/molpay.tpl';
+            }	
+		
+            $this->render();
     }
 	
 	/* return url */
     public function callback() {
-	  
+ 
         $this->load->model('checkout/order');
 
         $merchantid = $this->config->get('molpay_merchantid');
@@ -72,12 +73,14 @@ class ControllerPaymentMolpay extends Controller {
 
         $order_info = $this->model_checkout_order->getOrder($this->request->post['orderid']); // orderid
 
+        $this->model_checkout_order->confirm($this->request->post['orderid'], $this->config->get('molpay_order_status_id'));
+        
         if ( $status == "00" )  {
             $this->model_checkout_order->update($orderid , $this->config->get('molpay_success_status_id'), 'MP Normal Return', true);
             $this->redirect(HTTP_SERVER . 'index.php?route=checkout/success');
         } else {
-            $this->model_checkout_order->update($orderid, $this->config->get('molpay_failed_status_id'), 'MP Normal Return', true);
-
+            $this->model_checkout_order->update($orderid , $this->config->get('molpay_failed_status_id'), 'MP Normal Return', true);
+            
             $this->data['continue'] = $this->url->link('checkout/cart');
 
             if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/payment/molpay_fail.tpl')) {
@@ -110,16 +113,20 @@ class ControllerPaymentMolpay extends Controller {
         $key0 = md5($tranID.$orderid.$status.$domain.$amount.$currency);
         $key1 = md5($paydate.$merchantid.$key0.$appcode.$verifykey);
 
-        if ( $skey != $key1 ) $status = -1 ;
+        if ( $skey != $key1 )
+            $status = -1 ;
 
         if ($nbcb == 1) {
             echo "CBTOKEN:MPSTATOK";
-
             $order_info = $this->model_checkout_order->getOrder($this->request->post['orderid']); // orderid
-
-            if ( $status == "00" ) {
-                $this->model_checkout_order->update($orderid, $this->config->get('molpay_success_status_id'), 'MP Callback Return', true);
-            } else { 
+            
+            //Confirm the order If not created yet
+            $this->model_checkout_order->confirm($this->request->post['orderid'], $this->config->get('molpay_order_status_id'));
+            
+            if ( $status == "00" ) {                
+                $this->model_checkout_order->update($orderid , $this->config->get('molpay_success_status_id'), 'MP Callback Return', true);
+            }
+            else { 
                 $this->model_checkout_order->update($orderid, $this->config->get('molpay_failed_status_id'), 'MP Callback Return', true);
             }
         }
